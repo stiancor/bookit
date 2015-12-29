a9556b3a280372ad65dd3cd891b630100ce41179
stian.alsos@gmail.com <stian.alsos@gmail.com>
2015-12-30 00:13:49 +0100

    Fix typo

--- grails-app/controllers/no/alsos/bookit/UserController.groovy
+++ grails-app/controllers/no/alsos/bookit/UserController.groovy
@@ -7,7 +7,7 @@ class UserController {
     def create() {}
 
     def insert() {
-        log.info("Trying to create new user with emaiL: ${params.email}")
+        log.info("Trying to create new user with email: ${params.email}")
         def pubId = userService.createUser(params.email, params.fullName, params.phone, params.password)
         redirect(action: 'show', id: pubId)
     }
