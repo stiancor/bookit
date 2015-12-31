3c3e84bf8f6c39622d14c63c7dbb6a13d08c0e5f
stian.alsos@gmail.com <stian.alsos@gmail.com>
2015-12-31 14:39:38 +0100

    Add role method

--- grails-app/services/no/alsos/bookit/db/RoleService.groovy
+++ grails-app/services/no/alsos/bookit/db/RoleService.groovy
@@ -7,11 +7,14 @@ class RoleService {
     def dbService
 
     def create(String roleName) {
-        def pubId = Peer.squuid()
         dbService.conn.transact([[
                                          ':db/id'         : Peer.tempid(":db.part/user"),
                                          ':role/authority': roleName
                                  ]]).get()
-        pubId
+    }
+
+    def addRole(String userPublicId, role) {
+        def userId = Peer.query("[:find ?id . :where [?id :user/publicId ?publicId]]", dbService.db, UUID.fromString(userPublicId))
+        dbService.conn.transact([[':db/id': userId, ':user/role': [':role/authority': role]]]).get()
     }
 }
