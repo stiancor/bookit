bae8ad39cce9833cc9e05f10d94949e0240a0227
stian.alsos@gmail.com <stian.alsos@gmail.com>
2016-02-01 00:05:07 +0100

    Add a nice looking login page

--- grails-app/assets/javascripts/application.js
+++ grails-app/assets/javascripts/application.js
@@ -20,3 +20,13 @@ if (typeof jQuery !== 'undefined') {
         });
     })(jQuery);
 }
+
+$(document).ready(function () {
+    $('.forgot-pass').click(function(event) {
+        $(".pr-wrap").toggleClass("show-pass-reset");
+    });
+
+    $('.pass-reset-submit').click(function(event) {
+        $(".pr-wrap").removeClass("show-pass-reset");
+    });
+});
